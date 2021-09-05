b19af86b71d7560a7bb7248246b0d02659477c88
yangzhipenggit <1837520163@qq.com>
2021-09-05 16:40:31 +0800

    tesT

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@ProjectFile：PyCharm->untitled -> test
+@Author     ：yzp
+@Date       ：2021/9/5 14:52
+=================================================="""
+from yzp import Yzp
+print "这是一个测试"
+for i in range(1,10):
+    print ("这是第"+str(i)+"次测试")
+print "测试完毕！"
+t=Yzp()
+t.test()
+print "yzp"
+
---
+++ yzp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""=================================================
+@ProjectFile：PyCharm->untitled -> yzp
+@Author     ：yzp
+@Date       ：2021/9/5 15:46
+=================================================="""
+class Yzp:
+    def test(self):
+        lists=list()
+        for i in range(5):
+            #print "yzp-test"+str(i)
+            #lists=list()
+            print i
+            lists.append(i)
+        print lists
+        print 23
